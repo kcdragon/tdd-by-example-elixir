4045a09b8e96f569e9d0d9d4c9f99d89254f9a05
Mike Dalton <mdalton@hubtran.com>
2021-08-11 09:52:52 -0400

    finished chapter 8

--- lib/money.ex
+++ lib/money.ex
@@ -1,5 +1,5 @@
 defmodule Money do
   def equals(money, other_money) do
-    money.amount == other_money.amount
+    money.amount == other_money.amount && money.__struct__ == other_money.__struct__
   end
 end
