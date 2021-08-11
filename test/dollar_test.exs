8c8b0a839ec23dea7f75e7279df4b1d1eb14a4b4
Mike Dalton <mdalton@hubtran.com>
2021-08-10 21:16:01 -0400

    finished chapter 2

--- test/dollar_test.exs
+++ test/dollar_test.exs
@@ -6,5 +6,8 @@ defmodule DollarTest do
     five = %Dollar{amount: 5}
     ten = Dollar.times(five, 2)
     assert 10 == ten.amount
+
+    fifteen = Dollar.times(five, 3)
+    assert 15 == fifteen.amount
   end
 end
